045d838ff0de01f52fe7bc6b44eb19a53ee6cb33
sudoku-jm <sudoku8386@gmail.com>
2023-06-12 19:03:11 +0900

    first commit

---
+++ frontend/src/api.js
@@ -0,0 +1,10 @@
+const API_ENDPOINT =
+  "http://localhost:4001";
+
+const api = {
+  fetchCats: keyword => {
+    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then(res =>
+      res.json()
+    );
+  }
+};
